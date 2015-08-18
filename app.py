6bea4e7ddac242a09abace38574dbb1ad567137a
Henning Jacobs <henning@jacobs1.de>
2015-08-18 19:40:56 +0200

    update accounts in multiple threads

---
+++ app.py
@@ -0,0 +1,97 @@
+#!/usr/bin/env python3
+import gevent.monkey
+
+gevent.monkey.patch_all()
+
+import botocore
+import collections
+import connexion
+import datetime
+import flask
+import logging
+import os
+import requests
+import scan
+import threading
+import time
+import tokens
+
+from connexion import NoContent
+
+tokens.manage('tok', ['uid'])
+
+# HTTP connection pooling
+adapter = requests.adapters.HTTPAdapter(pool_connections=100, pool_maxsize=100)
+_session = requests.Session()
+_session.mount('http://', adapter)
+_session.mount('https://', adapter)
+requests = _session
+
+class BackgroundAccountThread(threading.Thread):
+    def __init__(self, account_ids):
+        threading.Thread.__init__(self, daemon=True)
+        self.account_ids = account_ids
+
+    def run(self):
+        while True:
+            try:
+                for acc in self.account_ids:
+                    for region in ['eu-west-1']:
+                        for i in range(3):
+                            try:
+                                scan.update_connections(acc, region)
+                                break
+                            except botocore.exceptions.ClientError:
+                                logging.exception('Client error')
+                                # throttling
+                                time.sleep(60)
+            except:
+                logging.exception('Failed to update')
+            time.sleep(30)
+
+class BackgroundThread(threading.Thread):
+    def __init__(self):
+        threading.Thread.__init__(self, daemon=True)
+
+    def run(self):
+        logging.info('Updating accounts..')
+        scan.update_accounts()
+        logging.info('Updating addresses..')
+        scan.update_addresses()
+        account_ids = collections.defaultdict(list)
+        for acc in scan.ACCOUNTS:
+            account_ids[hash(acc) % 8].append(acc)
+
+        for ids in account_ids.values():
+            thread = BackgroundAccountThread(ids)
+            thread.start()
+
+
+def get_accounts():
+    return scan.ACCOUNTS
+
+
+def get_connections():
+    res = {}
+    for key, val in scan.CONNECTIONS.items():
+        res['/'.join(key)] = get_connections_by_account(key[0], key[1])
+    return res
+
+
+def get_connections_by_account(account_id, region):
+    scan.REQUESTED_ACCOUNTS.add((account_id, region))
+    res = []
+    for conn, count in scan.CONNECTIONS.get((account_id, region), collections.Counter()).most_common():
+        res.append(list(conn) + [count])
+    return res
+
+
+if __name__ == '__main__':
+    logging.basicConfig(level=logging.INFO)
+    # the following line is only needed for OAuth support
+    api_args = {'tokeninfo_url': os.environ.get('HTTP_TOKENINFO_URL')}
+    app = connexion.App(__name__, port=8080, debug=True, server='gevent')
+    app.add_api('swagger.yaml', arguments=api_args)
+    bg_thread = BackgroundThread()
+    bg_thread.start()
+    app.run()
--- scan.py
+++ scan.py
@@ -5,8 +5,12 @@ from clickclick import Action, info
 import datetime
 import boto3
 import ipaddress
+import logging
+import os
+import requests
 import socket
 import sys
+import tokens
 import yaml
 from flowlogs_reader import FlowLogsReader
 
@@ -14,6 +18,24 @@ STUPS_CIDR = ipaddress.ip_network('172.31.0.0/16')
 
 AZ_NAMES_BY_REGION = {}
 NAMES = {}
+ACCOUNTS = {}
+CONNECTIONS = {}
+
+REQUESTED_ACCOUNTS = set()
+
+LAST_TIMES = {}
+
+def update_accounts():
+    r = requests.get(os.environ.get('HTTP_TEAM_SERVICE_URL') + '/api/accounts/aws', headers={'Authorization': 'Bearer {}'.format(tokens.get('tok'))})
+    ACCOUNTS.update({a['id']: a for a in r.json()})
+
+
+def update_addresses():
+    config = {'regions': ['eu-west-1'],
+              'accounts': {a['name']: {'domain': os.getenv('DOMAIN'), 'regions': ['eu-west-1']} for a in ACCOUNTS.values()}}
+    res = get_trusted_addresses(config)
+    NAMES.update(res)
+
 
 def get_az_names(region: str):
     names = AZ_NAMES_BY_REGION.get(region)
@@ -47,7 +69,7 @@ def get_trusted_addresses(config: dict):
             for domain in sorted(domains):
                 with Action('Checking {}'.format(domain)) as act:
                     try:
-                        ai = socket.getaddrinfo(domain, 443, family=socket.AF_INET, type=socket.SOCK_STREAM)
+                        ai = socket.getaddrinfo(domain, 443, family=socket.AF_INET, socktype=socket.SOCK_STREAM)
                     except:
                         ai = []
                         act.error('n/a')
@@ -59,77 +81,151 @@ def get_trusted_addresses(config: dict):
     return addresses
 
 
-try:
-    with open(sys.argv[2]) as fd:
-        NAMES = yaml.load(fd)
-except:
-    with open(sys.argv[1]) as fd:
-        config = yaml.load(fd)
-    addresses = get_trusted_addresses(config)
-    NAMES.update(addresses)
+def update_connections(account_id, region):
+    conn = CONNECTIONS.get((account_id, region), collections.Counter())
+    CONNECTIONS[(account_id, region)] = conn
+    get_connections(account_id, region, conn)
 
-with open(sys.argv[2], 'w') as fd:
-    yaml.dump(NAMES, fd)
+def get_connections(account_id, region, connections=None):
+    logging.info('Getting connections for {} {}..'.format(account_id, region))
+    sts = boto3.client('sts')
+    response = sts.assume_role(
+            RoleArn='arn:aws:iam::' + account_id + ':role/fullstop',
+            RoleSessionName='fullstop',
+            DurationSeconds=900)
+
+    session = boto3.Session(aws_access_key_id=response['Credentials']['AccessKeyId'],
+                      aws_secret_access_key=response['Credentials']['SecretAccessKey'],
+                      aws_session_token=response['Credentials']['SessionToken'],
+                      region_name=region
+                      )
+
+    ec2 = session.resource('ec2')
+    ec2_client = session.client('ec2')
+
+    instance_ids = []
+    interfaces = {}
+
+    with Action('Collecting network interfaces..'):
+        res = ec2_client.describe_network_interfaces()
+        for iface in res['NetworkInterfaces']:
+            if 'Association' in iface:
+                # public IP involved
+                interfaces[iface['NetworkInterfaceId']] = iface
+            if 'Attachment' in iface and 'InstanceId' in iface['Attachment']:
+                instance_ids.append(iface['Attachment']['InstanceId'])
+
+    instance_count = 0
+    with Action('Collecting public EC2 instances..'):
+        res = ec2_client.describe_instances(InstanceIds=instance_ids)
+        for reservation in res['Reservations']:
+            for inst in reservation['Instances']:
+                instance_count += 1
+                if 'PublicIpAddress' in inst and inst['PublicIpAddress'] not in NAMES and 'Tags' in inst:
+                    NAMES[inst['PublicIpAddress']] = ''.join([x['Value'] for x in inst['Tags'] if x['Key'] == 'Name'])
+                if 'PrivateIpAddress' in inst and 'Tags' in inst:
+                    NAMES[inst['PrivateIpAddress']] = ''.join([x['Value'] for x in inst['Tags'] if x['Key'] == 'Name'])
+
+    logging.info('Got {} interfaces and {} instances'.format(len(interfaces), instance_count))
+
+    connections = collections.Counter() if connections is None else connections
+    now = datetime.datetime.utcnow()
+    start_time = LAST_TIMES.get(account_id, now - datetime.timedelta(minutes=10))
+    reader = FlowLogsReader('VPCFlowLogs', region_name=region, start_time=start_time, end_time=now, interfaces=interfaces.keys())
+    record_count = 0
+    for record in reader:
+        if record.action == 'ACCEPT':
+            record_count += 1
+            src = ipaddress.ip_address(record.srcaddr)
+            if record.interface_id in interfaces and src not in STUPS_CIDR:
+                dst = ipaddress.ip_address(record.dstaddr)
+                name = NAMES.get(record.srcaddr, record.srcaddr)
+                dest = interfaces.get(record.interface_id).get('Description') or NAMES.get(record.dstaddr, record.dstaddr)
+                if 'NAT' not in dest and 'Odd' not in dest:
+                    conn = (name, dest, record.dstport)
+                    if conn not in connections:
+                        print(' '.join(map(str, conn)))
+                    connections[conn] += 1
+    logging.info('Got {} records'.format(record_count))
+    LAST_TIMES[account_id] = now
+    return connections
 
 
-ec2 = boto3.resource('ec2')
-ec2_client = boto3.client('ec2')
+def main():
+    try:
+        with open(sys.argv[2]) as fd:
+            NAMES = yaml.load(fd)
+    except:
+        with open(sys.argv[1]) as fd:
+            config = yaml.load(fd)
+        addresses = get_trusted_addresses(config)
+        NAMES.update(addresses)
 
-elb = boto3.client('elb')
-
-
-res = elb.describe_load_balancers()
-for lb in res['LoadBalancerDescriptions']:
-    if lb['Scheme'] == 'internet-facing':
-        for sg_id in lb['SecurityGroups']:
-            sg = ec2.SecurityGroup(sg_id)
-            allow_all = False
-            for rule in sg.ip_permissions:
-                for ip_range in rule['IpRanges']:
-                    if ip_range['CidrIp'] == '0.0.0.0/0':
-                        allow_all = True
-                        break
-            if allow_all:
-                info('LB {} allows traffic from everywhere'.format(lb['DNSName']))
-
-instance_ids = []
-interfaces = {}
-
-with Action('Collecting network interfaces..'):
-    res = ec2_client.describe_network_interfaces()
-    for iface in res['NetworkInterfaces']:
-        if 'Association' in iface:
-            # public IP involved
-            interfaces[iface['NetworkInterfaceId']] = iface
-        if 'InstanceId' in iface['Attachment']:
-            instance_ids.append(iface['Attachment']['InstanceId'])
-
-with Action('Collecting public EC2 instances..'):
-    res = ec2_client.describe_instances(InstanceIds=instance_ids)
-    for reservation in res['Reservations']:
-        for inst in reservation['Instances']:
-            if 'PublicIpAddress' in inst and inst['PublicIpAddress'] not in NAMES:
-                NAMES[inst['PublicIpAddress']] = ''.join([x['Value'] for x in inst['Tags'] if x['Key'] == 'Name'])
-            if 'PrivateIpAddress' in inst:
-                NAMES[inst['PrivateIpAddress']] = ''.join([x['Value'] for x in inst['Tags'] if x['Key'] == 'Name'])
+    with open(sys.argv[2], 'w') as fd:
+        yaml.dump(NAMES, fd)
 
 
-connections = collections.Counter()
-now = datetime.datetime.utcnow()
-start_time = now - datetime.timedelta(minutes=60)
-reader = FlowLogsReader('VPCFlowLogs', region_name='eu-west-1', start_time=start_time)
-for record in reader:
-    if record.action == 'ACCEPT':
-        src = ipaddress.ip_address(record.srcaddr)
-        if record.interface_id in interfaces and src not in STUPS_CIDR:
-            dst = ipaddress.ip_address(record.dstaddr)
-            name = NAMES.get(record.srcaddr, record.srcaddr)
-            dest = interfaces.get(record.interface_id).get('Description') or NAMES.get(record.dstaddr, record.dstaddr)
-            if 'NAT' not in dest and 'Odd' not in dest:
-                conn = (name, dest, record.dstport)
-                if conn not in connections:
-                    print(' '.join(map(str, conn)))
-                connections[conn] += 1
+    ec2 = boto3.resource('ec2')
+    ec2_client = boto3.client('ec2')
 
-for conn, count in connections.most_common(50):
-    print(' '.join(map(str, conn)), count)
+    elb = boto3.client('elb')
+
+
+    res = elb.describe_load_balancers()
+    for lb in res['LoadBalancerDescriptions']:
+        if lb['Scheme'] == 'internet-facing':
+            for sg_id in lb['SecurityGroups']:
+                sg = ec2.SecurityGroup(sg_id)
+                allow_all = False
+                for rule in sg.ip_permissions:
+                    for ip_range in rule['IpRanges']:
+                        if ip_range['CidrIp'] == '0.0.0.0/0':
+                            allow_all = True
+                            break
+                if allow_all:
+                    info('LB {} allows traffic from everywhere'.format(lb['DNSName']))
+
+    instance_ids = []
+    interfaces = {}
+
+    with Action('Collecting network interfaces..'):
+        res = ec2_client.describe_network_interfaces()
+        for iface in res['NetworkInterfaces']:
+            if 'Association' in iface:
+                # public IP involved
+                interfaces[iface['NetworkInterfaceId']] = iface
+            if 'Attachment' in iface and 'InstanceId' in iface['Attachment']:
+                instance_ids.append(iface['Attachment']['InstanceId'])
+
+    with Action('Collecting public EC2 instances..'):
+        res = ec2_client.describe_instances(InstanceIds=instance_ids)
+        for reservation in res['Reservations']:
+            for inst in reservation['Instances']:
+                if 'PublicIpAddress' in inst and inst['PublicIpAddress'] not in NAMES and 'Tags' in inst:
+                    NAMES[inst['PublicIpAddress']] = ''.join([x['Value'] for x in inst['Tags'] if x['Key'] == 'Name'])
+                if 'PrivateIpAddress' in inst and 'Tags' in inst:
+                    NAMES[inst['PrivateIpAddress']] = ''.join([x['Value'] for x in inst['Tags'] if x['Key'] == 'Name'])
+
+
+    connections = collections.Counter()
+    now = datetime.datetime.utcnow()
+    start_time = now - datetime.timedelta(minutes=60)
+    reader = FlowLogsReader('VPCFlowLogs', region_name='eu-west-1', start_time=start_time)
+    for record in reader:
+        if record.action == 'ACCEPT':
+            src = ipaddress.ip_address(record.srcaddr)
+            if record.interface_id in interfaces and src not in STUPS_CIDR:
+                dst = ipaddress.ip_address(record.dstaddr)
+                name = NAMES.get(record.srcaddr, record.srcaddr)
+                dest = interfaces.get(record.interface_id).get('Description') or NAMES.get(record.dstaddr, record.dstaddr)
+                if 'NAT' not in dest and ('Odd' not in dest or record.dstport == 22):
+                    conn = (name, dest, record.dstport)
+                    if conn not in connections:
+                        print(' '.join(map(str, conn)))
+                    connections[conn] += 1
+
+    for conn, count in connections.most_common(50):
+        print(' '.join(map(str, conn)), count)
+
+if __name__ == '__main__':
+    main()
